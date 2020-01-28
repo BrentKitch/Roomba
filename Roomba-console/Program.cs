4da91d7262ef7e8832d0c280e0e3883dcf95a1aa
Brent Kitchen <brent.kitchen@outlook.com>
2020-01-28 16:16:24 -0500

    Updated 'Roomba' to bounce out at the same angle it bounced in. Has bugs. Work in Progress.

--- Roomba-console/Program.cs
+++ Roomba-console/Program.cs
@@ -6,16 +6,17 @@ namespace Roomba_console
     {
         static void Main(string[] args)
         {
-            if (args.Length == 1)
-            {
-                var maxIters = int.Parse(args[0]);
+            //if (args.Length == 1)
+            //{
+            //    var maxIters = int.Parse(args[0]);
 
-                RunGameEngine(maxIters);
-            }
-            else
-            {
-                Console.WriteLine("Error: Please provide a single parameter representing the maximum number of iterations to run the simulation");
-            }
+            //    RunGameEngine(maxIters);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Error: Please provide a single parameter representing the maximum number of iterations to run the simulation");
+            //}
+            RunGameEngine(500000);
         }
 
         private static void RunGameEngine(int maxIterations)
@@ -30,14 +31,15 @@ namespace Roomba_console
             bool done = false;
             int iteration = 0;
 
-            double RoombaX;
-            double RoombaY;
-            int RoombaAngle;
+            double RoombaX = 0;
+            double RoombaY = 0;
+            int RoombaAngle = 40;
 
             var prng = new Random();
 
-            InitializeWorld(out RoombaX, out RoombaY, out RoombaAngle, prng);
-
+            //InitializeWorld(out RoombaX, out RoombaY, out RoombaAngle, prng);
+            //Console.WriteLine("Starting Angle");
+            //Console.WriteLine(RoombaAngle);
             var world = (RoombaX, RoombaY, RoombaAngle);
             OutputPosition(world);
 
@@ -52,6 +54,7 @@ namespace Roomba_console
                 DisplayWorld(world, collided);
 
                 done = iteration >= maxIterations;
+
             } while (!done);
         }
 
@@ -68,6 +71,8 @@ namespace Roomba_console
         private static void DisplayWorld((double, double, int) world
                                         , bool collided)
         {
+            //Console.WriteLine($"{ world.Item1} , { world.Item2}");
+            //Console.WriteLine(world.Item3);
             if (collided)
             {
                 // Output the coordinates
@@ -131,16 +136,82 @@ namespace Roomba_console
 
             var (RoombaX, RoombaY, RoombaAngle) = world;
             var RoombaAngleRadians = (RoombaAngle * Math.PI) / 180;
-
-            var newX = RoombaX + 0.01 * Math.Sin(RoombaAngleRadians);
-            var newY = RoombaY + 0.01 * Math.Cos(RoombaAngleRadians);
-
+          
+            var newX = RoombaX + ( 0.01 * Math.Cos(RoombaAngleRadians));
+            var newY = RoombaY + ( 0.01 * Math.Sin(RoombaAngleRadians));
+            //Console.WriteLine("The new X,Y values");
+            //Console.WriteLine($"{ newX} , { newY}");
+            //Console.WriteLine(RoombaAngle);
             var collided = InCollision(newX, newY);
-
+            
             if (collided)
             {
-                // Set the angle to a random angle
-                var NewAngle = prng.Next(0, 360);
+                // Set the angle 
+                int NewAngle;
+                if ( (RoombaAngle > 0 && RoombaAngle < 90) && (newX > 12 || ((newX > 1 && newY > 5) && (newX <= 4 && newY <= 8)) || (newX > 1 && newY > 8)
+                  && (newX <= 9 && newY <= 11))) // right wall quadrent 1 with sectional
+                {
+                   // Console.WriteLine(RoombaAngle);
+                   // Console.WriteLine("RW Q1");
+                    NewAngle = 180 - RoombaAngle;
+                    //Console.WriteLine(NewAngle);
+                }
+                else if ((RoombaAngle > 270 && RoombaAngle < 360) && (newX > 12 || ((newX > 1 && newY > 5) && (newX <= 4 && newY <= 8)) || (newX > 1 && newY > 8)
+                  && (newX <= 9 && newY <= 11))) // right wall quadrent 4
+                {
+                   // Console.WriteLine(RoombaAngle);
+                   // Console.WriteLine("RW Q4");
+                    NewAngle = 180 + (360 - RoombaAngle);
+                    //Console.WriteLine(NewAngle);
+                }
+                else if ((RoombaAngle > 180 && RoombaAngle < 270) && (newY < 0 || ((newX > 1 && newY > 5) && (newX <= 4 && newY <= 8)) || (newX > 1 && newY > 8)
+                  && (newX <= 9 && newY <= 11))) // bottom wall quadrent 3
+                {
+                   // Console.WriteLine(RoombaAngle);
+                   // Console.WriteLine("BW Q3");
+                    NewAngle = 90 + (270 - RoombaAngle);
+                  //  Console.WriteLine(NewAngle);
+                }
+                else if ((RoombaAngle> 270 && RoombaAngle < 360) && (newY < 0 || ((newX > 1 && newY > 5) && (newX <= 4 && newY <= 8)) || (newX > 1 && newY > 8)
+                  && (newX <= 9 && newY <= 11))) // bottom wall quadrent 4
+                {
+                   // Console.WriteLine(RoombaAngle);
+                  //  Console.WriteLine("BW Q4");
+                    NewAngle = 360 - RoombaAngle;
+                   // Console.WriteLine(NewAngle);
+                }
+                else if ((RoombaAngle > 0 && RoombaAngle < 90) && (newY > 12 || ((newX > 1 && newY > 5) && (newX <= 4 && newY <= 8)) || (newX > 1 && newY > 8)
+                  && (newX <= 9 && newY <= 11))) // upper wall quadrent 1
+                {
+                   // Console.WriteLine($"{RoombaX} , {RoombaY}");
+                   // Console.WriteLine(RoombaAngle);
+                  //  Console.WriteLine("UW Q1");
+                    NewAngle = 270 + (90 - RoombaAngle);
+                   // Console.WriteLine(NewAngle);
+                }
+                else if ((RoombaAngle> 90 && RoombaAngle < 180) && (newY > 12 || ((newX > 1 && newY > 5) && (newX <= 4 && newY <= 8)) || (newX > 1 && newY > 8)
+                  && (newX <= 9 && newY <= 11))) // upper wall quadrent 2
+                {
+                   // Console.WriteLine(RoombaAngle);
+                   // Console.WriteLine("UW Q2");
+                    NewAngle = 270 - (RoombaAngle - 90 );
+                   // Console.WriteLine(NewAngle);
+                }
+                else if ((RoombaAngle > 90 && RoombaAngle < 180) && (newX < 0 || ((newX > 1 && newY > 5) && (newX <= 4 && newY <= 8)) || (newX > 1 && newY > 8)
+                  && (newX <= 9 && newY <= 11))) // left wall quadrent 2
+                {
+                   // Console.WriteLine(RoombaAngle);
+                  //  Console.WriteLine("LW Q2");
+                    NewAngle = 180 - (RoombaAngle);
+                  //  Console.WriteLine(NewAngle);
+                }
+                else  //left wall quadrent 3
+                {
+                   // Console.WriteLine(RoombaAngle);
+                   // Console.WriteLine("LW Q3");
+                    NewAngle = 360 - (RoombaAngle - 180);
+                   // Console.WriteLine(NewAngle);
+                }
                 newWorld = (RoombaX, RoombaY, NewAngle);
             }
             else
